af8a16c45cd683b103d8c609fce3bad41c2dbd7b
Louis <loulamia@Louiss-Eximchain-MacBook-Pro.local>
2019-04-17 11:09:30 -0400

    Delete revoke-https-cert.sh

--- packer/instance-scripts/revoke-https-cert.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -u -o pipefail
-
-function clear_ssl_certs {
-    sudo certbot revoke --delete-after-revoke --reason superseded --cert-path /etc/letsencrypt/archive/guardian/cert1.pem
-}
-
-clear_ssl_certs
